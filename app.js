73f3f32fb71d4107f866753554acf56196e5276c
Marcus Hsu <mh3346@columbia.edu>
2017-12-06 12:03:35 -0500

    Refactor html template

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const packageJson = require('./package.json');
+const queryString = require('query-string');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const queryString = require('query-string');
 
 
 const app = express();
-app.set('views', __dirname + '/views');
+
+app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 const ip = process.env.IP || packageJson.config.ip;
 const port = process.env.PORT || packageJson.config.port;
 const appleAppSiteAssociationContent = fs.readFileSync(path.resolve(__dirname, './public/', 'apple-app-site-association'));
@@ -18,22 +20,19 @@ app.get('/apple-app-site-association', (req, res) => {
   return res.status(200).send(appleAppSiteAssociationContent);
 });
 
-app.get('/', function (req, res) {
-  let myURL = 'https://circlus.herokuapp.com/'
-  const query = queryString.stringify(req.query);
-  if(query) {
-  	myURL += `?${query}`;
-  }
-
-  res.render('index', { myURL })
-})
-
 app.use(express.static(path.resolve(__dirname, 'public/')));
 
 // All routes should direct to the index.html
 app.route('/*')
   .get((req, res) => {
-    res.sendFile(path.resolve(__dirname, './public/', 'index.html'));
+    let smartAppBannerArgument = 'https://circlus.herokuapp.com/';
+    const query = queryString.stringify(req.query);
+
+    if (query) {
+      smartAppBannerArgument += `?${query}`;
+    }
+
+    return res.render('index', { smartAppBannerArgument });
   });
 
 const server = app.listen(port, ip, () => {
